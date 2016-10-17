a00f72cfdf91fc4676747d7ab4a11b1a9b0296ad
crystaltangtsj <crytaltang@pku.edu.cn>
2016-10-17 12:46:57 +0800

    a test

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,7 +49,7 @@ LANG_DISPLAY = {
     'zh': '中文'
 }
 
-PLUGIN_PATHS = ['/Users/laser/pelicanplugins/pelican-plugins/']
+PLUGIN_PATHS = ['/home/crystal/pelican-plugins/']
 PLUGINS = ['render_math', 'pelican-toc']
 
 TOC = {
